80341134d273d43e0d00f1d52b1eb76f1f35b5e7
ym <ouyngmin@gmail.com>
2023-01-20 14:19:19 +0900

    overlay log file

--- app/src/main/java/com/choi/getmeminfo/MainActivity.java
+++ app/src/main/java/com/choi/getmeminfo/MainActivity.java
@@ -1,19 +1,27 @@
 package com.choi.getmeminfo;
 
 import android.app.ActivityManager;
+import android.app.AppOpsManager;
+import android.app.usage.UsageEvents;
+import android.app.usage.UsageStatsManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
+import android.util.LongSparseArray;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -33,12 +41,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static Context mContext;
     private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
     private TextView textView, textView2, textView3, textView4;
     private Button sv_on_bt, sv_off_bt;
     private boolean roof = false;
+    boolean operation = false;
+    CheckPackageNameThread checkPackageNameThread;
+    public static String folderpath = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,8 +58,32 @@ public class MainActivity extends AppCompatActivity {
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        mContext = this.getApplicationContext();
         setUi();
-        //getMemInfo();
+/*        getMemInfo();
+        checkPermission();
+        Intent PermissionIntent = new Intent(Settings.ACTION_USAGE_ACCESS_SETTINGS, Uri.parse("package:" + getPackageName()));
+        startActivity(PermissionIntent);
+        operation = true;
+        checkPackageNameThread = new CheckPackageNameThread();
+        checkPackageNameThread.start();
+
+        PackageManager pm = this.getPackageManager();
+        boolean installed = false;
+        List<PackageInfo> packages = pm.getInstalledPackages(0);
+        for (PackageInfo packageInfo : packages)  {
+            Log.e("YYYM", "PackageManager: "+packageInfo.packageName
+            + ", versionName: "+packageInfo.versionName
+            + ", sourceDir:: "+packageInfo.applicationInfo.sourceDir);
+            Log.d("Test", "packageName: ${info.packageName}"
+                    + ", versionName: ${info.versionName}"
+                    + ", lastUpdateTime: ${info.lastUpdateTime}"
+                    + ", targetSdk: ${info.applicationInfo.targetSdkVersion}"
+                    + ", minSdk: ${info.applicationInfo.minSdkVersion}"
+                    + ", sourceDir: ${info.applicationInfo.sourceDir}"
+                    + ", uid: ${info.applicationInfo.uid}"
+                    + ", label: ${info.applicationInfo.loadLabel(packageManager)}")
+        }*/
     }
 
     private void setUi() {
@@ -57,11 +93,14 @@ public class MainActivity extends AppCompatActivity {
         textView4 = findViewById(R.id.textView4);
         sv_on_bt = findViewById(R.id.bt_on);
         sv_off_bt = findViewById(R.id.bt_off);
-        onProccessCheck();
+        //MyService myService = new MyService(mContext);
+        folderpath = getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        //onProccessCheck();
         sv_on_bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                checkPermissionAndStartService();
+                startService(new Intent(MainActivity.this, MyService.class));
+                //checkPermissionAndStartService();
             }
         });
     }
@@ -97,13 +136,22 @@ public class MainActivity extends AppCompatActivity {
     {
         ActivityManager am = (ActivityManager) getApplicationContext().getSystemService(Context.ACTIVITY_SERVICE);
         List<ActivityManager.RunningAppProcessInfo> appList = am.getRunningAppProcesses();
+
+        List<ActivityManager.RecentTaskInfo> recentTasks = am.getRecentTasks(5, ActivityManager.RECENT_WITH_EXCLUDED);
+        for (ActivityManager.RecentTaskInfo task : recentTasks) {
+            Intent taskIntent = task.baseIntent;
+            Log.d("Recent Task", "Intent: " + taskIntent);
+            Log.d("Recent Task", "Persistent ID: " + task.persistentId);
+            Log.d("Recent Task", "ID: " + task.id);
+        }
         String name = "";
-        Log.e("YYYM", "onProccessCheck: ...................." );
+        Log.e("YYYM", "onProccessCheck: ....................");
         for (int i=0; i<appList.size(); i++) {
             ActivityManager.RunningAppProcessInfo rapi = appList.get(i);
             name += "#"+i+"_"+rapi.processName;
 
-            Log.e("YYYM", "onProccessCheck: "+rapi.importance);
+            Log.e("YYYM", "rapi.processName: "+rapi.processName);
+            Log.e("YYYM", "rapi.importance: "+rapi.importance);
         }
         textView4.setText(name);
     }
@@ -175,5 +223,106 @@ public class MainActivity extends AppCompatActivity {
             getMemInfo();
             if (roof == false) {break;}
         }*/
+    }
+
+
+    // 현재 포그라운드 앱 패키지 로그로 띄우는 함수
+    private class CheckPackageNameThread extends Thread{
+
+        public void run(){
+            // operation == true 일때만 실행
+            while(operation){
+
+                // 현재 포그라운드 앱 패키지 이름 가져오기
+                //System.out.println(getPackageName(getApplicationContext()));
+                Log.e("YYYM", "run: "+getPackageName(getApplicationContext()) );
+                try {
+                    // 2초마다 패키치 이름을 로그창에 출력
+                    sleep(2000);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 현재 포그라운드 앱 패키지를 가져오는 함수
+    public static String getPackageName(@NonNull Context context) {
+
+        // UsageStatsManager 선언
+        UsageStatsManager usageStatsManager = (UsageStatsManager) context.getSystemService(Context.USAGE_STATS_SERVICE);
+
+        // 마지막 실행 앱 타임스탬프
+        long lastRunAppTimeStamp = 0L;
+
+        // 얼마만큼의 시간동안 수집한 앱의 이름을 가져오는지 정하기 (begin ~ end 까지의 앱 이름을 수집한다)
+        final long INTERVAL = 1000 * 60 * 5;
+        final long end = System.currentTimeMillis();
+        final long begin = end - INTERVAL; // 5분전
+
+
+        LongSparseArray packageNameMap = new LongSparseArray<>();
+
+        // 수집한 이벤트들을 담기 위한 UsageEvents
+        final UsageEvents usageEvents = usageStatsManager.queryEvents(begin, end);
+
+        // 이벤트가 여러개 있을 경우 (최소 존재는 해야 hasNextEvent가 null이 아니니까)
+        while (usageEvents.hasNextEvent()) {
+
+            // 현재 이벤트를 가져오기
+            UsageEvents.Event event = new UsageEvents.Event();
+            usageEvents.getNextEvent(event);
+
+            // 현재 이벤트가 포그라운드 상태라면(현재 화면에 보이는 앱이라면)
+            //if(isForeGroundEvent(event)) {
+
+                // 해당 앱 이름을 packageNameMap에 넣는다.
+                packageNameMap.put(event.getTimeStamp(), event.getPackageName());
+
+                // 가장 최근에 실행 된 이벤트에 대한 타임스탬프를 업데이트 해준다.
+                if(event.getTimeStamp() > lastRunAppTimeStamp) {
+                    lastRunAppTimeStamp = event.getTimeStamp();
+                }
+            //}
+        }
+        // 가장 마지막까지 있는 앱의 이름을 리턴해준다.
+        return packageNameMap.get(lastRunAppTimeStamp, "").toString();
+    }
+
+    // 앱이 포그라운드 상태인지 체크
+    private static boolean isForeGroundEvent(UsageEvents.Event event) {
+
+        // 이벤트가 없으면 false 반환
+        if(event == null)
+            return false;
+
+        // 이벤트가 포그라운드 상태라면 true 반환
+        if(BuildConfig.VERSION_CODE >= 29)
+            return event.getEventType() == UsageEvents.Event.ACTIVITY_RESUMED;
+
+        return event.getEventType() == UsageEvents.Event.MOVE_TO_FOREGROUND;
+    }
+
+    // 권환 체크
+    private boolean checkPermission(){
+
+        boolean granted = false;
+
+        AppOpsManager appOps = (AppOpsManager) getApplicationContext()
+                .getSystemService(Context.APP_OPS_SERVICE);
+
+        int mode = appOps.checkOpNoThrow(AppOpsManager.OPSTR_GET_USAGE_STATS,
+                android.os.Process.myUid(), getApplicationContext().getPackageName());
+
+        if (mode == AppOpsManager.MODE_DEFAULT) {
+            granted = (getApplicationContext().checkCallingOrSelfPermission(
+                    android.Manifest.permission.PACKAGE_USAGE_STATS) == PackageManager.PERMISSION_GRANTED);
+        }
+        else {
+            granted = (mode == AppOpsManager.MODE_ALLOWED);
+        }
+
+        return granted;
     }
 }
--- app/src/main/java/com/choi/getmeminfo/MyService.java
+++ app/src/main/java/com/choi/getmeminfo/MyService.java
@@ -1,20 +1,189 @@
 package com.choi.getmeminfo;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.PixelFormat;
+import android.graphics.Point;
+import android.os.Build;
+import android.os.Environment;
 import android.os.IBinder;
+import android.util.Log;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
-public class MyService extends Service {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MyService extends Service implements View.OnTouchListener{
+    private String TAG = "MyService";
+
+    private View onTopView;
+    private WindowManager manager;
+    public Context mContext;
+
+    private boolean stopThread = false;
+
+    private String foldername = "TESTLOG";
+    private String filename = "logfile.txt";
+
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
 
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e("YYYM", "onCreate: jkashfjklhasjkfajdhfkjlshdf");
+        LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        onTopView = inflater.inflate(R.layout.always_on_top_layout, null);
+        onTopView.setOnTouchListener(this);
+
+        int LAYOUT_FLAG;
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY;
+        }else{
+            LAYOUT_FLAG = WindowManager.LayoutParams.TYPE_PHONE;
+        }
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                LAYOUT_FLAG,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL|
+                        WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH,
+                PixelFormat.TRANSLUCENT);
+
+        params.gravity = Gravity.LEFT | Gravity.TOP;
+
+        manager = (WindowManager) getSystemService(WINDOW_SERVICE);
+        manager.addView(onTopView, params);
+
+        Button closeBtn = onTopView.findViewById(R.id.close_this_window);
+        closeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                manager.removeView(onTopView);
+                onTopView = null;
+                stopThread = true;
+                stopSelf();
+            }
+        });
+        //Log.e("YYYM", "getLogcatLog: "+getLogcatLog());
+        BackLogThread thread = new BackLogThread();
+        thread.setDaemon(true);
+        thread.start();
+        //getLogcatLog();
+    }
+
+    float xpos = 0;
+    float ypos = 0;
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        int action = motionEvent.getAction();
+        int pointerCount = motionEvent.getPointerCount();
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                if (pointerCount == 1) {
+                    xpos = motionEvent.getRawX();
+                    ypos = motionEvent.getRawY();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                if (pointerCount == 1) {
+                    WindowManager.LayoutParams lp = (WindowManager.LayoutParams) view.getLayoutParams();
+                    float dx = xpos - motionEvent.getRawX();
+                    float dy = ypos - motionEvent.getRawY();
+                    xpos = motionEvent.getRawX();
+                    ypos = motionEvent.getRawY();
+
+                    Log.d(TAG, "lp.x : " + lp.x + ", dx : " + dx + "lp.y : " + lp.y + ", dy : " + dy);
+
+                    lp.x = (int) (lp.x - dx);
+                    lp.y = (int) (lp.y - dy);
+
+                    manager.updateViewLayout(view,lp);
+                    return true;
+                }
+                break;
+
+        }
+        return false;
+    }
 
 
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (onTopView != null) {
+            manager.removeView(onTopView);
+            onTopView = null;
+        }
+    }
 
+    private void getLogcatLog() {
+        StringBuilder log = new StringBuilder();
+        try {
+            //Process process = Runtime.getRuntime().exec("logcat -d -v time");
+            Process process = Runtime.getRuntime().exec("logcat -v time");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
 
+            String folderPath = MainActivity.folderpath;
+            String filePath = folderPath + File.separator + filename;
+            //Log.e("YYYM", "filePath: "+filePath);
+            File logFile = new File(filePath);
+            if(!new File(folderPath).exists()) {
+                new File(folderPath).mkdir();
+            }
+            logFile.createNewFile();
+            String line = "";
+            //파일 output stream 생성
+            FileOutputStream fos = new FileOutputStream(logFile, true);
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(fos));
+            while ((line = bufferedReader.readLine()) != null) {
+                writer.write(line);
+                writer.write("\n");
+            }
+            writer.flush();
+            fos.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            stopThread = true;
+        }
+        Log.e("YYYM", "getLogcatLog: " + log.toString());
+        //return log.toString();
+    }
+
+    class BackLogThread extends Thread {
+        @Override
+        public void run() {
+         while (!stopThread)
+         {
+             getLogcatLog();
+         }
+            Log.e("YYYM", "쓰레드 종료: ");
+        }
+    }
 }
